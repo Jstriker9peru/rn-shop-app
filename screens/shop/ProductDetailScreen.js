755e9943620fdc93b444efc0f7cf049b19245537
Jesp <jstriker9peru@gmail.com>
2020-05-27 16:58:07 -0400

    extra

---
+++ screens/shop/ProductDetailScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+const ProductDetailScreen = () => {
+    return (
+        <FlatList />
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+
+    }
+});
+
+export default ProductDetailScreen;
