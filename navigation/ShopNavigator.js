345d5c51b622b7ae899630fffa0316d6e91a08b0
Jesp <jstriker9peru@gmail.com>
2020-05-28 08:56:00 -0400

    added product styling

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,14 +1,23 @@
 import { Platform } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import Colors from '../constants/Colors';
 
 const ProductsNavigator = createStackNavigator({
-    ProductsOverview: ProductsOverviewScreen
+    ProductsOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen
 }, {
     defaultNavigationOptions: {
         headerStyle: {
             backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+        },
+        headerTitleStyle: {
+            fontFamily: 'open-sans-bold'
+        },
+        headerBackTitleStyle: {
+            fontFamily: 'open-sans'
         },
         headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
     }
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,20 +1,37 @@
 import React from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
 import { useSelector } from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
 
-const ProductsOverviewScreen = () => {
+const ProductsOverviewScreen = ({ navigation }) => {
     const products = useSelector((state) => state.products.availableProducts);
     return (
         <FlatList
             data={products}
-            renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+            renderItem={(itemData) => {
+                const { id, imageUrl, title, price } = itemData.item;
+                return (
+                    <ProductItem
+                        image={imageUrl}
+                        title={title}
+                        price={price}
+                        onViewDetail={() => {
+                            navigation.navigate("ProductDetail", {
+                                productId: id,
+                                productTitle: title
+                            });
+                        }}
+                        onAddToCart={() => {}}
+                    />
+                );
+            }}
         />
     );
 };
 
 ProductsOverviewScreen.navigationOptions = {
-    headerTitle: 'All Products'
-}
+    headerTitle: "All Products",
+};
 
 const styles = StyleSheet.create({
     screen: {},
